967ec2623d62569693d9145c5b5051738542c49a
mitra-tech <55147150+mitra-tech@users.noreply.github.com>
2021-02-01 23:36:39 -0700

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
+const options = [
+  {
+    label: "The Color Red",
+    value: "Red",
+  },
+  {
+    label: "The Color Blue",
+    value: "Blue",
+  },
+  {
+    label: "The Shade Of Green",
+    value: "Green",
+  },
+];
+
+const items = [
+  {
+    title: "whats react?",
+    content: "react is a front-end js framework.",
+  },
+
+  {
+    title: "why use react?",
+    content: "react is a js favorite library amongst engineers.",
+  },
+
+  {
+    title: "how do you use react?",
+    content: "you use react by creating components.",
+  },
+];
+
+export default () => {
+  const[selected , setSelected] = useState(options[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    <div>
+    <Header/>
+    <Route  path= '/'>
+    <Accordion items= {items}/>
+    </Route>
 
-export default App;
+    <Route  path= '/list'>
+    <Search />
+    </Route>
+
+    <Route  path= '/dropdown'>
+    <Dropdown 
+    label='select a color'
+    options= {options}
+    selected= {selected}
+    onSelectedChange = {setSelected}
+    />
+    </Route>
+
+    <Route  path= '/translate'>
+    <Translate />
+    </Route>
+
+</div>
+  );
+};
+
+
+
+
